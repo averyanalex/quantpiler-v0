37ae64f59fd0168efe99b4c1328a828566fceb15
AveryanAlex <alex@averyan.ru>
2022-09-22 21:52:20 +0300

    Full adder

--- quantpiler/adder.py
+++ quantpiler/adder.py
@@ -2,10 +2,49 @@
 Quantum adder circuit generator.
 """
 
-from typing import Union, List, Dict
-
-from qiskit import QuantumRegister
+from qiskit import QuantumRegister, AncillaRegister
 from qiskit.circuit import QuantumCircuit
+
+
+def new_full_adder() -> QuantumCircuit:
+    """Generate full adder circuit.
+
+    Returns:
+        QuantumCircuit: Full adder circuit.
+    """
+    a = QuantumRegister(1, name="a")
+    b = QuantumRegister(1, name="b")
+    sm = QuantumRegister(1, name="sum")
+    c = QuantumRegister(1, name="cout")
+
+    qc = QuantumCircuit(a, b, sm, c, name="full_adder")
+
+    qc.ccx(a, b, c)
+    qc.cx(a, b)
+    qc.ccx(b, sm, c)
+    qc.cx(b, sm)
+    qc.cx(a, b)
+
+    return qc
+
+
+def new_inplace_full_adder() -> QuantumCircuit:
+    a = QuantumRegister(1, name="a")
+    b = QuantumRegister(1, name="b")
+    c = QuantumRegister(1, name="c")
+    anc = AncillaRegister(1)
+
+    qc = QuantumCircuit(a, b, c, anc, name="inplace_full_adder")
+
+    qc.ccx(a, b, anc)
+    qc.cx(a, b)
+    qc.ccx(b, c, anc)
+    qc.cx(b, c)
+    qc.cx(a, b)
+    qc.swap(b, c)
+    qc.swap(c, anc)
+
+    return qc
 
 
 def new_adder(size: int) -> QuantumCircuit:
@@ -17,20 +56,15 @@ def new_adder(size: int) -> QuantumCircuit:
     Returns:
         QuantumCircuit: Generated adder.
     """
-
     a = QuantumRegister(size, name="a")
     b = QuantumRegister(size, name="b")
     sm = QuantumRegister(size, name="sum")
 
     qc = QuantumCircuit(a, b, sm, name="adder")
+    full_adder = new_full_adder()
 
     for i in range(size - 1):
-        qc.ccx(a[i], b[i], sm[i + 1])
-        qc.cx(a[i], b[i])
-        qc.ccx(b[i], sm[i], sm[i + 1])
-        qc.cx(b[i], sm[i])
-        qc.cx(a[i], b[i])
-        qc.barrier()
+        qc.compose(full_adder, qubits=[a[i], b[i], sm[i], sm[i + 1]], inplace=True)
 
     qc.cx(a[size - 1], sm[size - 1])
     qc.cx(b[size - 1], sm[size - 1])
